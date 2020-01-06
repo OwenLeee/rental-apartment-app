ccadd34890186a34766c59f83edf9e0ef883a73c
Harry Chow <harryccpongg@gmail.com>
2020-01-06 21:41:54 +0800

    modify knexfile

--- back-end/knexfile.ts
+++ back-end/knexfile.ts
@@ -8,8 +8,15 @@ module.exports = {
     client: "sqlite3",
     connection: {
       database: process.env.DB_NAME,
-      user:     process.env.DB_USERNAME,
+      user: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
     }
   },
 
@@ -17,7 +24,7 @@ module.exports = {
     client: "postgresql",
     connection: {
       database: process.env.DB_NAME,
-      user:     process.env.DB_USERNAME,
+      user: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD
     },
     pool: {
@@ -33,7 +40,7 @@ module.exports = {
     client: "postgresql",
     connection: {
       database: process.env.DB_NAME,
-      user:     process.env.DB_USERNAME,
+      user: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD
     },
     pool: {
