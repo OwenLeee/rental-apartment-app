23c51e1be8b3ebded2a03c80a2aed32ebed9cd44
Harry Chow <harryccpongg@gmail.com>
2020-01-07 19:54:25 +0800

    modify knexfile

--- back-end/knexfile.ts
+++ back-end/knexfile.ts
@@ -23,9 +23,9 @@ module.exports = {
   staging: {
     client: "postgresql",
     connection: {
-      database: process.env.DB_NAME,
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD
+      database: process.env.POSTGRES_DB,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD
     },
     pool: {
       min: 2,
@@ -39,9 +39,9 @@ module.exports = {
   production: {
     client: "postgresql",
     connection: {
-      database: process.env.DB_NAME,
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD
+      database: process.env.POSTGRES_DB,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD
     },
     pool: {
       min: 2,
