772ef400009b1efaaf8b0c54d884f8f0531a36f9
Harry Chow <harryccpongg@gmail.com>
2020-01-13 01:06:57 +0800

    change CI/CD

--- back-end/knexfile.ts
+++ back-end/knexfile.ts
@@ -40,7 +40,7 @@ module.exports = {
   production: {
     client: "postgresql",
     connection: {
-      host: process.env.POSTGRES_HOST ,
+      host: process.env.POSTGRES_HOST,
       database: process.env.POSTGRES_DB,
       user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD
